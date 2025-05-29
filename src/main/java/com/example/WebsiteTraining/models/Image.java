65ad408e7266e7ebf0a394eea8034dbbf9b258e5
PochkaVirta <cyberdog771@gmail.com>
2025-05-29 05:25:14 +0300

    improved readability

--- src/main/java/com/example/WebsiteTraining/models/Image.java
+++ src/main/java/com/example/WebsiteTraining/models/Image.java
@@ -13,17 +13,11 @@ import lombok.NoArgsConstructor;
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
     private Long id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "originalFileName")
     private String originalFileName;
-    @Column(name = "size")
     private Long size;
-    @Column(name = "contentType")
     private String contentType;
-    @Column(name = "isPreviewImage")
     private boolean isPreviewImage;
     @Lob
     private byte[] bytes;
--- src/main/java/com/example/WebsiteTraining/models/Product.java
+++ src/main/java/com/example/WebsiteTraining/models/Product.java
@@ -17,17 +17,11 @@ import java.util.List;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
     private long id;
-    @Column(name = "title")
     private String title;
-    @Column(name = "description", columnDefinition = "text")
     private String description;
-    @Column(name = "price")
     private int price;
-    @Column(name = "city")
     private String city;
-    @Column(name = "author")
     private String author;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
     private List<Image> images = new ArrayList<>();
